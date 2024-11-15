217493b39858207db1f0fee75a735a8c3b8708c1
jhotlann <19623263@std.stei.itb.ac.id>
2024-11-15 15:06:01 +0700

    feat:stack.c

---
+++ src/stack.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "stack.h"
+#include "boolean.h"
+
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty(Stack *S) {
+    Top(*S) = Nil;
+}
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty(Stack S) {
+    return Top(S) == Nil;
+}
+
+boolean IsFull(Stack S) {
+    return Top(S) == MaxEl - 1;
+}
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push(Stack * S, infotype X) {
+    if (!IsFull(*S)) {
+        Top(*S)++;
+        InfoTop(*S) = X;
+    } 
+}
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop(Stack * S, infotype* X) {
+    if (!IsEmpty(*S)) {
+        *X = InfoTop(*S);
+        Top(*S)--;
+    } 
+}
