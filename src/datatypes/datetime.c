184180ee61b8021c34803d70c6c4c0e4924384ec
iqbalhaidr <13523111@std.stei.itb.ac.id>
2024-11-17 11:02:49 +0700

    feat: ADT Datetime

---
+++ src/datatypes/datetime.c
@@ -0,0 +1,56 @@
+#include "datetime.h"
+
+/* *** KONSTRUKTOR *** */
+void CreateDatetime(Datetime *t) {
+    DAY(*t) = NIL;
+    MONTH(*t) = NIL;
+    YEAR(*t) = NIL;
+    HOUR(*t) = NIL;
+    MINUTE(*t) = NIL;
+    SECOND(*t) = NIL;
+    EPOCH(*t) = NIL;
+}
+
+/* *** TES KOSONG *** */
+boolean isEmptyDatetime(Datetime t) {
+    return (DAY(t) == NIL && MONTH(t) == NIL 
+            && YEAR(t) == NIL && HOUR(t) == NIL 
+            && MINUTE(t) == NIL && SECOND(t) == NIL 
+            && EPOCH(t) == NIL);
+}
+
+/* *** MENCATAT WAKTU TERKINI *** */
+void getDatetime(Datetime *t) {
+    time_t now = time(NULL);
+    //time_t time(time_t *x) 
+    //both assign to *x and return, seconds since Unix Epoch
+    if (now == (time_t)(-1)) {
+        return;
+    }
+
+    //tm* localtime(time_t *x) convert seconds to readable-format
+    struct tm *tm_info = localtime(&now);
+    if (tm_info == NULL) {
+        return;
+    }
+
+    t->day = tm_info->tm_mday;
+    t->month = tm_info->tm_mon + 1;      // tm_mon ranges 0 to 11
+    t->year = tm_info->tm_year + 1900;   // tm_year years since 1900
+    t->hour = tm_info->tm_hour;
+    t->minute = tm_info->tm_min;
+    t->second = tm_info->tm_sec;
+    t->epoch = (int)now;                 // epoch adalah jumlah detik sejak UNIX EPOCH
+}
+
+/* *** KONVERSI *** */
+int toSecond(Datetime t) {
+    return EPOCH(t);
+}
+
+/* *** DISPLAY *** */
+void displayDatetime(Datetime t) {
+    printf("%02d-%02d-%d @ %02d.%02d", DAY(t), MONTH(t), YEAR(t)
+                                    , HOUR(t), MINUTE(t));
+}
+
--- src/modules/datetime.h
+++ src/modules/datetime.h
@@ -11,13 +11,15 @@
 
 #define NIL -1
 
-/* *** Definisi *** */
+/* *** DEFINISI *** */
 typedef struct {
     int day;
     int month;
     int year;
     int hour;
     int minute;
+    int second;
+    int epoch;
 } Datetime;
 
 /* ********* AKSES (Selektor) ********* */
@@ -27,71 +29,33 @@ typedef struct {
 #define YEAR(t)   (t).year
 #define HOUR(t)   (t).hour
 #define MINUTE(t) (t).minute
+#define SECOND(t) (t).second
+#define EPOCH(t)  (t).epoch
 
-/* *** Konstruktor *** */
+/* *** KONSTRUKTOR *** */
+void CreateDatetime(Datetime *t);
 /* I.S. Sembarang */
 /* F.S. Sebuah t kosong, dimana semua komponen t bernilai NIL */
 /* Proses : Membuat t kosong sesuai definisi */
-void CreateDatetime(Datetime *t) {
-    DAY(*t) = NIL;
-    MONTH(*t) = NIL;
-    YEAR(*t) = NIL;
-    HOUR(*t) = NIL;
-    MINUTE(*t) = NIL;
-}
 
+/* *** TES KOSONG *** */
+boolean isEmptyDatetime(Datetime t);
 /* Mengirim true jika t kosong : lihat definisi di atas */
-boolean isEmptyDatetime(Datetime t) {
-    return (DAY(t) == NIL && MONTH(t) == NIL && YEAR(t) == NIL 
-            && HOUR(t) == NIL && MINUTE(t) == NIL);
-}
 
-/* *** Mencatat Waktu Terkini *** */
+/* *** MENCATAT WAKTU TERKINI *** */
+void getDatetime(Datetime *t);
 /* I.S. t kosong */
 /* F.S. t berisi waktu terkini */
 /* Proses : Mengambil waktu terkini dan memasukkannya ke t */
-void getDatetime(Datetime *t) {
-    time_t now = time(NULL);
-    //time_t time(time_t *x) 
-    //both assign to *x and return, seconds since Unix Epoch
-    if (now == (time_t)(-1)) {
-        return;
-    }
 
-    //tm* localtime(time_t *x) convert seconds to readable-format
-    struct tm *tm_info = localtime(&now);
-    if (tm_info == NULL) {
-        return;
-    }
+/* *** KONVERSI *** */
+int toSecond(Datetime t);
+/* Mengembalikan detik sejak Unix Epoch sampai t */
 
-    t->day = tm_info->tm_mday;
-    t->month = tm_info->tm_mon + 1;      // tm_mon ranges 0 to 11
-    t->year = tm_info->tm_year + 1900;   // tm_year years since 1900
-    t->hour = tm_info->tm_hour;
-    t->minute = tm_info->tm_min;
-}
-
-/* *** Display *** */
+/* *** DISPLAY *** */
+void displayDatetime(Datetime t);
 /* I.S. t tidak boleh kosong */
 /* F.S. Menulis waktu dengan format DD-MM-YYYY @ HH.MM  */
 /* Proses : Menulis waktu sesuai format dari data t */
-void displayDatetime(Datetime t) {
-    printf("%02d-%02d-%d @ %02d.%02d", DAY(t), MONTH(t), YEAR(t)
-                                     , HOUR(t), MINUTE(t));
-}
 
 #endif
-
-
-/* DRIVER
-
-#include "datetime.h"
-
-int main() {
-    Datetime t;
-    CreateDatetime(&t);
-    getDatetime(&t);
-    displayDatetime(t);
-
-    return 0;
-}
