738107c8ae151dbe7e1dc98e3c03457d92c84c0d
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-12-01 16:50:39 +0700

    feat: add modules integrated import

---
+++ src/modules.h
@@ -0,0 +1,15 @@
+#ifndef MODULES_H
+#define MODULES_H
+
+#include "./modules/boolean.h"
+#include "./modules/mesinkarakter.h"
+#include "./modules/mesinkata.h"
+
+#include "./modules/listuser.h"
+#include "./modules/listemail.h"
+
+#include "./program/load.h"
+#include "./pengguna/pengguna.h"
+#include "./program/program.h"
+
+#endif
