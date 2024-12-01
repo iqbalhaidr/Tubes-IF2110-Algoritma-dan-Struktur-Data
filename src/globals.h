165753a78ea4e91d6683b5b8e9856784128e81ad
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-12-01 16:49:42 +0700

    feat: add global variables

---
+++ src/globals.h
@@ -0,0 +1,11 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include "modules.h"
+
+extern ListUser listUser;
+extern ListEmail listEmail; 
+extern activeUser user;
+extern boolean authenticated;
+
+#endif
