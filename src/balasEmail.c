a76e964f1369064fb6a47679512a453fbadc99e9
jhotlann <19623263@std.stei.itb.ac.id>
2024-12-06 16:58:07 +0700

    fix: bug nomor reply, email tidak ditemukan atau email orang lain

--- src/balasEmail.c
+++ src/balasEmail.c
@@ -4,8 +4,9 @@
 #include "./datatypes/listdin.c"
 #include "draftemail.c"
 
+// Fungsi untuk membentuk emailtree dengan menambah semua reply ke subjek utama
 void addReplies(Tree node, ListEmail listEmail) {
-    for (int i = 0; i < listEmail.number; i++) {
+    for (int i = 0; i < listEmail.number; i++) { // Mencari semua reply dari email
         if (listEmail.data[i].reply == node->info) {
             Address newNode = newTreeNode(listEmail.data[i].id);
             if (newNode != NULL) {
@@ -18,17 +19,17 @@ void addReplies(Tree node, ListEmail listEmail) {
                 } else {
                     printf("Error: Node is full\n");
                 }
-            }
-            
+            }  
         }
     }
 }
 
+// Fungsi untuk mencari indeks email berdasarkan emailID
 IdxType indexOfEmail(ListEmail l, int emailID) {
     boolean found;
     IdxType i = 0;
    
-    if(isEmptyEmail(l)){
+    if(isEmptyEmail(l)){ // Jika list kosong
         return IDX_UNDEF;
     } else{
         found = 0;
@@ -40,15 +41,15 @@ IdxType indexOfEmail(ListEmail l, int emailID) {
                 i++;
             }
         }
-
         if(found){
             return i;
-        } else{
+        } else{ // Jika tidak ditemukan
             return IDX_UNDEF;
         }
     }
 }
 
+// Fungsi untuk mencari indeks email root dari emailID
 IdxType getRoot (ListEmail l, int emailID){
     IdxType i = indexOfEmail(l, emailID);
     while (l.data[i].reply != -1){
@@ -57,6 +58,7 @@ IdxType getRoot (ListEmail l, int emailID){
     return i;
 }
 
+// Fungsi untuk membuat list preorder dari emailTree
 ListDin createPreorderList(Tree p) {
     ListDin l;
     CreateListDin(&l, 100);
@@ -76,6 +78,7 @@ ListDin createPreorderList(Tree p) {
     }
 }
 
+// Fungsi untuk mencetak email head
 void printEmailHead(ListEmail ListEmail,Tree emailTree,int emailID){
     emailType email = ELMT_EMAIL(ListEmail, indexOfEmail(ListEmail, emailID));
     printf("[----------------------------[ Baca Pesan ]---------------------------]\n");
@@ -86,6 +89,7 @@ void printEmailHead(ListEmail ListEmail,Tree emailTree,int emailID){
     printf("[---------------------------------------------------------------------]\n");
 }
 
+// Fungsi untuk mencetak email
 void printEmail(ListEmail ListEmail,Tree emailTree,int emailID){
     emailType email = ELMT_EMAIL(ListEmail, indexOfEmail(ListEmail, emailID));
     printf("\n[%s]\n",email.subyek);
@@ -94,21 +98,30 @@ void printEmail(ListEmail ListEmail,Tree emailTree,int emailID){
     printf("[---------------------------------------------------------------------]\n");
 }
 
+// Fungsi untuk mencari kedalaman dari suatu node
 int findDepth(Address root, int val, int depth) { 
-    if (root == NULL) { 
+    if (root == NULL) { // Jika root kosong
         return -1;  
     } 
     if (root->info == val) { 
         return depth; 
     } 
     int leftDepth = findDepth(root->left, val, depth + 1); 
-    if (leftDepth != -1) { 
+    if (leftDepth != -1) { // Jika node ditemukan di subtree kiri
         return leftDepth; 
     } 
     return findDepth(root->right, val, depth + 1);
 }
 
+// Fungsi untuk membaca email
 void readEmail(ListEmail listEmail, int emailID) {
+    if (indexOfEmail(listEmail, emailID) == IDX_UNDEF) {
+        printf("Email tidak ditemukan\n");
+        return;
+    } else if (listEmail.data[indexOfEmail(listEmail, emailID)].idPenerima != 0) {
+        printf("Email tidak diperuntukkan untuk Anda\n");
+        return;
+    }
 
     IdxType root = getRoot(listEmail, emailID);
     
@@ -124,17 +137,25 @@ void readEmail(ListEmail listEmail, int emailID) {
     }
 }
 
+// Fungsi untuk membalas email
 void replyEmail(int id_user, ListUser list_user, ListEmail listEmail, int id_reply) {
-
+    if (indexOfEmail(listEmail, id_reply) == IDX_UNDEF) {
+        printf("Email tidak ditemukan\n");
+        return;
+    } else if (listEmail.data[indexOfEmail(listEmail, id_reply)].idPenerima != id_user) {
+        printf("Email tidak diperuntukkan untuk Anda\n");
+        return;
+    }
+    
     IdxType root = getRoot(listEmail, id_reply);
     
     Tree emailTree = newTreeNode(listEmail.data[root].id);
 
     addReplies(emailTree, listEmail);
 
-    int reply = findDepth(emailTree, id_reply, 0);
+    int reply = findDepth(emailTree, id_reply, 0) + 1;
 
     int id_old = listEmail.data[root].id;
 
-    DraftEmail(id_user, list_user, listEmail, reply, id_reply, id_old);
+    DraftEmail(id_user, list_user, &listEmail, reply, id_reply, id_old);
 }
