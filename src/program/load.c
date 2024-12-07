de5451697570b3fbc00bf93e1fc09a98ba34a1fe
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-12-07 18:06:40 +0700

    fix: update  load using mesinkata

--- src/program/load.c
+++ src/program/load.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "load.h"
 #include "utility.h"
 #include "../globals.h"
@@ -36,156 +38,214 @@ int loadConfigfromFoler(char *folderPath, ListUser *listUser, ListEmail *listEma
 }
 
 /* Memuat list user */
-int loadUser(char* fileUser, ListUser *listUser) {
-    FILE *file = fopen(fileUser, "r");
-    char word[CAPACITY_CHAR_USER];
-
-    if (file == NULL) {
-        printf("File %s gagal dimuat!\n", fileUser);
-        return 0;
+int loadUser(char *filePath, ListUser *listUser){
+    CreateListUser(listUser);
+    char *id;
+    int suc = StartWordFromFile(filePath);
+    if (suc) {
+        char *count = toString(currentWord); //banyaknya user
+        listUser->number= stringToInt(count);
+        for (int i = 0; i < listUser->number; i++){
+            ADVNewLine();
+            id = toString(currentWord);
+            listUser->data[i].id = stringToInt(id);
+            ADVNewLine();
+            listUser->data[i].email = toString(currentWord);
+            ADVNewLine();
+            listUser->data[i].password = toString(currentWord);
+        }
+        return 1;
     }
-
-    /* Read First Line */
-    fscanf(file, "%5s", word);
-
-    int lineCount = 0;
-
-    while(fscanf(file, "%255s", word) == 1){
-        userType user;
-        user.id = stringToInt(word);
-
-        fscanf(file, "%255s", word);
-        user.email = store(word);
-
-        fscanf(file, "%255s", word);
-        user.password = store(word);
-
-        addUser(listUser, user);
-        lineCount++;
-    }
-    listUser->number = lineCount;
-    fclose(file);
-    return 1;
+    return 0;
+    
 }
 
 /* Memuat list email */
-int loadEmail(char* fileEmail, ListEmail *listEmail) {
-    FILE *file = fopen(fileEmail, "r");
-    char word[CAPACITY_CHAR_EMAIL];
+int loadEmail(char* filePath, ListEmail *listEmail) {
+    CreateListEmail(listEmail, 1000);
+    char *temp;
+    int suc = StartWordFromFile(filePath);
+    if (suc) {
+        char *count = toString(currentWord); //banyaknya email
+        int numemail = stringToInt(count);
+        emailType email;
 
-    if (file == NULL) {
-        printf("File %s gagal dimuat!\n", fileEmail);
+        for (int i = 0; i < numemail; i++){
+            ADVNewLine();
+            temp = toString(currentWord);
+            email.id = stringToInt(temp);
+            
+            ADVNewLine();
+            temp = toString(currentWord);
+            email.idPengirim = stringToInt(temp);
+
+            ADVNewLine();
+            temp = toString(currentWord);
+            email.idPenerima = stringToInt(temp);
+
+            ADVNewLine();
+            temp = toString(currentWord);
+            email.idCC = stringToInt(temp);
+
+            ADVNewLine();
+            email.timestamp = toString(currentWord);
+
+            ADVNewLine();
+            email.subyek = toString(currentWord);
+            while (currentWord.TabWord[currentWord.Length - 1] == '\\') {
+                email.subyek[currentWord.Length-1] = '\0';
+               
+                ADVNewLine();
+                email.subyek = concat(email.subyek, " ");
+                email.subyek = concat(email.subyek, toString(currentWord));
+            }
+
+            ADVNewLine();
+            email.body = toString(currentWord);
+            while (currentWord.TabWord[currentWord.Length - 1] == '\\') {
+                email.body[currentWord.Length-1] = '\0';
+               
+                ADVNewLine();
+                email.body = concat(email.body, " ");
+                email.body = concat(email.body, toString(currentWord));
+            }
+
+            ADVNewLine();
+            temp = toString(currentWord);
+            email.reply = stringToInt(temp);
+
+            ADVNewLine();
+            temp = toString(currentWord);
+            email.read = stringToInt(temp);
+
+            ADVNewLine();
+            temp = toString(currentWord);
+            email.readCC = stringToInt(temp);
+
+            addEmail(listEmail, email);
+        }
+        return 1;
+    }
+    return 0;
+}
+
+#ifdef _WIN32
+    #include <direct.h>
+#else
+    #include <unistd.h>
+#endif
+
+int isDirectoryExist(char *folderPath) {
+    struct stat info;
+    if (stat(folderPath, &info) != 0) {
+        printf("masuk sini1\n");
+        // Cannot access path, directory may not exist
+        return 0;
+    } else if (info.st_mode & S_IFDIR) {
+        // Path exists and is a directory
+        printf("masuk sini2\n");
+        return 1;
+    } else {
+        // Path exists but is not a directory
+        printf("masuk sini3\n");
         return 0;
     }
+}
 
-    /* Read First Line */
-    fscanf(file, "%1000[^\n]\n", word);
-
-    int lineCount = 0;
-    emailType email;
-
-    while(fscanf(file, "%1000[^\n]\n", word) != EOF){
-        email.id = stringToInt(word);
-
-        fscanf(file, "%1000[^\n]\n", word);  
-        email.idPengirim = stringToInt(word);
-
-        fscanf(file, "%1000[^\n]\n", word);
-        email.idPenerima = stringToInt(word);
-
-        fscanf(file, "%1000[^\n]\n", word);
-        email.idCC = stringToInt(word);
-
-        fscanf(file, "%1000[^\n]\n", word);
-        email.timestamp = store(word);
-
-        fscanf(file, "%1000[^\n]\n", word);
-        email.subyek = store(word);
-
-        fscanf(file, "%1000[^\n]\n", word);
-        //handle \\ for new line in email.config
-        email.body = store(word);
-
-        fscanf(file, "%1000[^\n]\n", word);
-        email.reply = stringToInt(word);
-
-        fscanf(file, "%1000[^\n]\n", word);
-        email.read = *store(word) - '0';
-
-        fscanf(file, "%1000[^\n]\n", word);
-        email.readCC = *store(word) - '0';
-
-        addEmail(listEmail, email);
-        lineCount++;
+int createDirectory(char *folderPath) {
+    if (!isDirectoryExist(folderPath)) {
+        #ifdef _WIN32
+            if (_mkdir(folderPath) == 0) {
+                printf("Folder berhasil dibuat!\n");
+                return 0; // Directory successfully created
+            } else {
+                printf("Gagal membuat folder.\n");
+                return -1; // Failed to create directory
+            }
+        #else
+            if (mkdir(folderPath, 0755) == 0) {
+                printf("Folder berhasil dibuat!\n");
+                return 0; // Directory successfully created
+            } else {
+                printf("Gagal membuat folder.\n");
+                return -1; // Failed to create directory
+            }
+        #endif
     }
-    listEmail->number = lineCount;
-    fclose(file);
-
-    return 1;
+    return 0; // Directory already exists
 }
 
 int saveConfig(){
     /* Save User */
-    FILE *fileUser = fopen(fileUserPath, "w");
-    if (fileUser == NULL){
+    char *folderPath; char fileSaveUserPath[256]; char fileSaveEmailPath[256];
+    printf("Masukkan nama folder penyimpanan: \n");
+    STARTWORD();
+
+    folderPath = toString(currentWord);
+
+    if (createDirectory(folderPath) != 0){
+        return 0;
+    }
+
+    snprintf(fileSaveUserPath, sizeof(fileSaveUserPath), "./%s/pengguna.config", folderPath);
+
+    FILE *fileSaveUser = fopen(fileSaveUserPath, "w");
+    if (fileSaveUser == NULL){
         printf("Gagal menyimpan konfigurasi :(\n");
         return 0;
     }
     //number of user
-    fprintf(fileUser, "%d\n", listUser.number);
+    fprintf(fileSaveUser, "%d\n", listUser.number);
     int k = 0;
     for (int i = 0; i <listUser.number; i++){
-        fprintf(fileUser, "%d\n", listUser.data[k].id);
-        fprintf(fileUser, "%s\n", listUser.data[k].email);
-        fprintf(fileUser, "%s\n", listUser.data[k].password);
+        fprintf(fileSaveUser, "%d\n", listUser.data[k].id);
+        fprintf(fileSaveUser, "%s\n", listUser.data[k].email);
+        fprintf(fileSaveUser, "%s", listUser.data[k].password);
+        if (i < listUser.number - 1){
+            fprintf(fileSaveUser, "\n");
+        }
         k++;
     }
-    fclose(fileUser);
+    fclose(fileSaveUser);
 
     /* Save Email */
-    FILE *fileEmail = fopen(fileEmailPath, "w");
-    if (fileEmail == NULL){
+
+    snprintf(fileSaveEmailPath, sizeof(fileSaveEmailPath), "./%s/email.config", folderPath);
+
+    FILE *fileSaveEmail = fopen(fileSaveEmailPath, "w");
+    if (fileSaveEmail == NULL){
         printf("Gagal menyimpan konfigurasi :(\n");
         return 0;
     }
     //number of email
-    fprintf(fileEmail, "%d\n", listEmail.number);
+    fprintf(fileSaveEmail, "%d\n", listEmail.number);
     k = 0;
     for (int i = 0; i <listEmail.number; i++){
-        fprintf(fileEmail, "%d\n", listEmail.data[k].id);
-        fprintf(fileEmail, "%d\n", listEmail.data[k].idPengirim);
-        fprintf(fileEmail, "%d\n", listEmail.data[k].idPenerima);
-        fprintf(fileEmail, "%d\n", listEmail.data[k].idCC);
-        fprintf(fileEmail, "%s\n", listEmail.data[k].timestamp);
-        fprintf(fileEmail, "%s\n", listEmail.data[k].subyek);
-        fprintf(fileEmail, "%s\n", listEmail.data[k].body);
-        fprintf(fileEmail, "%d\n", listEmail.data[k].reply);
-        fprintf(fileEmail, "%d\n", listEmail.data[k].read);
-        fprintf(fileEmail, "%d\n", listEmail.data[k].readCC);
+        fprintf(fileSaveEmail, "%d\n", listEmail.data[k].id);
+        fprintf(fileSaveEmail, "%d\n", listEmail.data[k].idPengirim);
+        fprintf(fileSaveEmail, "%d\n", listEmail.data[k].idPenerima);
+        fprintf(fileSaveEmail, "%d\n", listEmail.data[k].idCC);
+        fprintf(fileSaveEmail, "%s\n", listEmail.data[k].timestamp);
+        fprintf(fileSaveEmail, "%s\n", listEmail.data[k].subyek);
+        fprintf(fileSaveEmail, "%s\n", listEmail.data[k].body);
+        fprintf(fileSaveEmail, "%d\n", listEmail.data[k].reply);
+        fprintf(fileSaveEmail, "%d\n", listEmail.data[k].read);
+        fprintf(fileSaveEmail, "%d", listEmail.data[k].readCC);
+        if (i < listEmail.number - 1){
+            fprintf(fileSaveEmail, "\n");
+        }
         k++;
     }
-    fclose(fileEmail);
+    fclose(fileSaveEmail);
     printf("File konfigurasi telah disimpan!\n");
     return 1;
 }
 
-void readFileToArray(char* fileUser, ListUser *listUser){
-    FILE *file = fopen(fileUser, "r");
-    // char word[CAPACITY_CHAR_USER];
-
-    if (file == NULL) {
-        printf("File %s gagal dimuat!\n", fileUser);
-        // return 0;
-    }
-
-    STARTWORD();
-    char* readWord = toString(currentWord);
-    printf("%s\n", readWord);
-}
-
 /* DRIVER */
 // int main(){
-//     readFileToArray(fileUserPath, &listUser);
+//     ListUser listUser;
+//     CreateListUser(&listUser);
+//     loadDataTry(fileUserPath, &listUser);
 
 //     return 0;
 // }
